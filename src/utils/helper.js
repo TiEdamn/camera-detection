02e4466384d7eef2f799e42b1e88b549b4b1baca
Igor Tikhanovich <tikhanovich.89@gmail.com>
2020-01-09 10:55:04 +0300

    initial commit

---
+++ src/utils/helper.js
@@ -0,0 +1,342 @@
+export default {
+    // Maximum alert
+    person: {
+        ru: 'Человек',
+        color: '#e13535',
+        alert: true
+    },
+
+    // Transport
+    bicycle: {
+        ru: 'Велосипед',
+        color: '#6f75e3'
+    },
+    car: {
+        ru: 'Автомобиль',
+        color: '#6f75e3'
+    },
+    motorcycle: {
+        ru: 'Мотоцикл',
+        color: '#6f75e3'
+    },
+    airplane: {
+        ru: 'Самолет',
+        color: '#6f75e3'
+    },
+    bus: {
+        ru: 'Автобус',
+        color: '#6f75e3'
+    },
+    train: {
+        ru: 'Поезд',
+        color: '#6f75e3'
+    },
+    truck: {
+        ru: 'Грузовик',
+        color: '#6f75e3'
+    },
+    boat: {
+        ru: 'Лодка',
+        color: '#6f75e3'
+    },
+
+    // Static
+    'traffic light': {
+        ru: 'Светофор',
+        color: '#afafaf'
+    },
+    'fire hydrant': {
+        ru: 'Гидрант',
+        color: '#afafaf'
+    },
+    'stop sign': {
+        ru: 'Знак стоп',
+        color: '#afafaf'
+    },
+    'parking meter': {
+        ru: 'Парковочные часы',
+        color: '#afafaf'
+    },
+    bench: {
+        ru: 'Скамейка',
+        color: '#afafaf'
+    },
+
+    // Animals
+    bird: {
+        ru: 'Птица',
+        color: '#ecc320',
+        alert: true
+    },
+    cat: {
+        ru: 'Кот',
+        color: '#ecc320',
+        alert: true
+    },
+    dog: {
+        ru: 'Собака',
+        color: '#ecc320',
+        alert: true
+    },
+    horse: {
+        ru: 'Лошадь',
+        color: '#ecc320',
+        alert: true
+    },
+    sheep: {
+        ru: 'Овца',
+        color: '#ecc320',
+        alert: true
+    },
+    cow: {
+        ru: 'Корова',
+        color: '#ecc320',
+        alert: true
+    },
+    elephant: {
+        ru: 'Слон',
+        color: '#ecc320',
+        alert: true
+    },
+    bear: {
+        ru: 'Медведь',
+        color: '#ecc320',
+        alert: true
+    },
+    zebra: {
+        ru: 'Зебра',
+        color: '#ecc320',
+        alert: true
+    },
+    giraffe: {
+        ru: 'Жираф',
+        color: '#ecc320',
+        alert: true
+    },
+
+    // Things
+    backpack: {
+        ru: 'Рюкзак',
+        color: '#afafaf'
+    },
+    umbrella: {
+        ru: 'Зонт',
+        color: '#afafaf'
+    },
+    handbag: {
+        ru: 'Сумка',
+        color: '#afafaf'
+    },
+    tie: {
+        ru: 'Галстук',
+        color: '#afafaf'
+    },
+    suitcase: {
+        ru: 'Чемодан',
+        color: '#afafaf'
+    },
+    frisbee: {
+        ru: 'Фрисби',
+        color: '#afafaf'
+    },
+    skis: {
+        ru: 'Лыжи',
+        color: '#afafaf'
+    },
+    snowboard: {
+        ru: 'Сноуборд',
+        color: '#afafaf'
+    },
+    'sports ball': {
+        ru: 'Мяч',
+        color: '#afafaf'
+    },
+    kite: {
+        ru: 'Воздушный змей',
+        color: '#afafaf'
+    },
+    'baseball bat': {
+        ru: 'Бейсбольная бита',
+        color: '#afafaf'
+    },
+    'baseball glove': {
+        ru: 'Бейсбольная перчатка',
+        color: '#afafaf'
+    },
+    skateboard: {
+        ru: 'Скейтборд',
+        color: '#afafaf'
+    },
+    surfboard: {
+        ru: 'Доска для серфинга',
+        color: '#afafaf'
+    },
+    'tennis racket': {
+        ru: 'Теннисная ракетка',
+        color: '#afafaf'
+    },
+    bottle: {
+        ru: 'Бутылка',
+        color: '#afafaf'
+    },
+    'wine glass': {
+        ru: 'Стакан вина',
+        color: '#afafaf'
+    },
+    cup: {
+        ru: 'Кружка',
+        color: '#afafaf'
+    },
+    fork: {
+        ru: 'Вилка',
+        color: '#afafaf'
+    },
+    knife: {
+        ru: 'Нож',
+        color: '#afafaf'
+    },
+    spoon: {
+        ru: 'Ложка',
+        color: '#afafaf'
+    },
+    bowl: {
+        ru: 'Миска',
+        color: '#afafaf'
+    },
+    banana: {
+        ru: 'Банан',
+        color: '#afafaf'
+    },
+    apple: {
+        ru: 'Яблоко',
+        color: '#afafaf'
+    },
+    sandwich: {
+        ru: 'Бутерброд',
+        color: '#afafaf'
+    },
+    orange: {
+        ru: 'Апельсин',
+        color: '#afafaf'
+    },
+    broccoli: {
+        ru: 'Брокколи',
+        color: '#afafaf'
+    },
+    carrot: {
+        ru: 'Морковь',
+        color: '#afafaf'
+    },
+    'hot dog': {
+        ru: 'Хот дог',
+        color: '#afafaf'
+    },
+    pizza: {
+        ru: 'Пицца',
+        color: '#afafaf'
+    },
+    donut: {
+        ru: 'Пончик',
+        color: '#afafaf'
+    },
+    cake: {
+        ru: 'Торт',
+        color: '#afafaf'
+    },
+    chair: {
+        ru: 'Стул',
+        color: '#afafaf'
+    },
+    couch: {
+        ru: 'Диван',
+        color: '#afafaf'
+    },
+    'potted plant': {
+        ru: 'Растение в горшке',
+        color: '#afafaf'
+    },
+    bed: {
+        ru: 'Кровать',
+        color: '#afafaf'
+    },
+    'dining table': {
+        ru: 'Обеденный стол',
+        color: '#afafaf'
+    },
+    toilet: {
+        ru: 'Туалет',
+        color: '#afafaf'
+    },
+    tv: {
+        ru: 'Телевизор',
+        color: '#afafaf'
+    },
+    laptop: {
+        ru: 'Ноутбук',
+        color: '#afafaf'
+    },
+    mouse: {
+        ru: 'Мышь',
+        color: '#afafaf'
+    },
+    remote: {
+        ru: 'Пульт',
+        color: '#afafaf'
+    },
+    keyboard: {
+        ru: 'Клавиатура',
+        color: '#afafaf'
+    },
+    'cell phone': {
+        ru: 'Мобильный телефон',
+        color: '#afafaf'
+    },
+    microwave: {
+        ru: 'Микроволновка',
+        color: '#afafaf'
+    },
+    oven: {
+        ru: 'Печь',
+        color: '#afafaf'
+    },
+    toaster: {
+        ru: 'Тостер',
+        color: '#afafaf'
+    },
+    sink: {
+        ru: 'Раковина',
+        color: '#afafaf'
+    },
+    refrigerator: {
+        ru: 'Холодильник',
+        color: '#afafaf'
+    },
+    book: {
+        ru: 'Книга',
+        color: '#afafaf'
+    },
+    clock: {
+        ru: 'Часы',
+        color: '#afafaf'
+    },
+    vase: {
+        ru: 'Ваза',
+        color: '#afafaf'
+    },
+    scissors: {
+        ru: 'Ножницы',
+        color: '#afafaf'
+    },
+    'teddy bear': {
+        ru: 'Плюшевый медведь',
+        color: '#afafaf'
+    },
+    'hair drier': {
+        ru: 'Фен',
+        color: '#afafaf'
+    },
+    toothbrush: {
+        ru: 'Зубная щетка',
+        color: '#afafaf'
+    }
+};
